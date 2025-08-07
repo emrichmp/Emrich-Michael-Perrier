e8f28f6c6dac9f2fbf3629cc0b64e1f73a38c275
Emrich Perrier <emrichmp@gmail.com>
2025-08-07 13:51:50 -0700

    Removed Reel stuff for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router } from 'react-router-dom'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
-import DemoReel from './components/DemoReel'
-import About from './components/About'
-import Projects from './components/Projects'
-import Experience from './components/Experience'
-import Coaching from './components/Coaching'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import ScrollToTop from './components/ScrollToTop'
+import { BrowserRouter as Router } from "react-router-dom";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+// import DemoReel from './components/DemoReel'
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Coaching from "./components/Coaching";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
           <Navbar />
         </div>
         <main className="w-full overflow-x-hidden">
-          <DemoReel />
+          {/* <DemoReel /> */}
           <About />
           <Projects />
           <Experience />
@@ -30,7 +30,7 @@ function App() {
         <ScrollToTop />
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
